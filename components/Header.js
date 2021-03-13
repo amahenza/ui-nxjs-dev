c2c87d9859fc83e77ed4fe007d05a36f9a63d552
amahenza <80218681+amahenza@users.noreply.github.com>
2021-03-13 08:43:17 +0700

    Initial commit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => (
+  <div className="Header">
+    <style jsx>{`
+      background-color: transparent;
+      color: black;
+      width: 100%;
+      height: 50px;
+    `}</style>
+  </div>
+);
+
+export default Header;
---
+++ components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => (
+  <div className="NavBar">
+    NAVBAR
+    <style jsx>{`
+      background-color: white;
+      color: white;
+      width: 100%;
+      height: 60px;
+    `}</style>
+  </div>
+);
+
+export default NavBar;

2882cdfe0dc8b39f32a70c1d000a5c090fb7636b
amahenza <80218681+amahenza@users.noreply.github.com>
2021-03-15 06:48:29 +0700

    Handle skip function fixes VirtualTourDialog imp true condition {open && (  - to skip the dialog

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./Header";
 import NavBar from "./NavBar";
 import VirtualTourDialog from "./VirtualTourDialog";
@@ -5,49 +6,52 @@ import VirtualTourDialog from "./VirtualTourDialog";
 import navButtons from "../configs/navButtons";
 import tourButton from "../configs/tourButton";
 
-const Layout = (props) => (
-  <div className="Layout">
-    <VirtualTourDialog tourButton={tourButton} />
-    <Header />
-    <div className="Content">{props.children}</div>
-    <NavBar navButtons={navButtons} />
-    <style jsx global>{`
-      * {
-        box-sizing: border-box;
-      }
+function Layout(props) {
+  return (
+    <div className="Layout">
+      <VirtualTourDialog tourButton={tourButton} />
 
-      html,
-      body,
-      #__next {
-        height: 100%;
-        width: 100%;
-      }
+      <Header />
+      <div className="Content">{props.children}</div>
+      <NavBar navButtons={navButtons} />
+      <style jsx global>{`
+        * {
+          box-sizing: border-box;
+        }
 
-      body {
-        margin: 0;
-        padding: 0;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-          "Helvetica Neue", sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
+        html,
+        body,
+        #__next {
+          height: 100%;
+          width: 100%;
+        }
 
-      .Layout {
-        display: flex;
-        flex-direction: column;
-        height: 100%;
-        width: 100%;
-      }
+        body {
+          margin: 0;
+          padding: 0;
+          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+            "Helvetica Neue", sans-serif;
+          -webkit-font-smoothing: antialiased;
+          -moz-osx-font-smoothing: grayscale;
+        }
 
-      .Content {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        font-family: Arial;
-      }
-    `}</style>
-  </div>
-);
+        .Layout {
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+          width: 100%;
+        }
+
+        .Content {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          font-family: Arial;
+        }
+      `}</style>
+    </div>
+  );
+}
 
 export default Layout;
